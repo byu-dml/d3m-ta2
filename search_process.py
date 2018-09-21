b8318de36534a22f30f590e9f4ff196dcaba04c2
Sean George <seangeorge0@gmail.com>
2018-09-21 11:00:11 -0600

    Add list of search_solutions to search_process

--- search_process.py
+++ search_process.py
@@ -5,3 +5,4 @@ class SearchProcess(object):
     def __init__(self, search_id, request):
         self.search_id = search_id
         self.search_request = request
+        self.search_solutions = []
