83871bc1d95401711d3bc6a522d48bf673736ffc
Sean George <seangeorge0@gmail.com>
2018-09-24 13:11:58 -0600

    Fix parsing problem description

--- test/test_search_solutions.py
+++ test/test_search_solutions.py
@@ -18,8 +18,8 @@ class TestSearchSolutions:
         assert isinstance(response, core_pb2.SearchSolutionsResponse), 'call to SearchSolutions did not return an instance of SearchSolutionsResponse'
 
     @staticmethod
-    def test_search_solutions_response_search_id(stub: core_pb2_grpc.CoreStub, protocol_version: str):
-        response_1 = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version))
+    def test_search_solutions_response_search_id(stub: core_pb2_grpc.CoreStub, protocol_version: str, sick_problem: grpc_problem.ProblemDescription):
+        response_1 = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version, problem=sick_problem))
         if not hasattr(response_1, 'search_id'):
             pytest.fail("SearchSolutionsResponse does not contain attribute 'search_id'")
 
@@ -27,13 +27,13 @@ class TestSearchSolutions:
         if len(search_id_1) < 22:
             pytest.fail(f'search_id %s returned in SearchSolutionsResponse is either empty or less than 22 characters {search_id_1}')
 
-        response_2 = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version))
+        response_2 = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version, problem=sick_problem))
         search_id_2 = response_2.search_id
         assert search_id_1 != search_id_2, f'Two consecutive calls to SearchSolutions produced the same search_id: {search_id_1}'
 
     @staticmethod
-    def test_get_search_solutions_results_response(stub: core_pb2_grpc.CoreStub, protocol_version: str):
-        response = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version))
+    def test_get_search_solutions_results_response(stub: core_pb2_grpc.CoreStub, protocol_version: str, sick_problem: grpc_problem.ProblemDescription):
+        response = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version, problem=sick_problem))
         search_id = response.search_id
         request = core_pb2.GetSearchSolutionsResultsRequest(search_id=search_id)
         try:
@@ -44,7 +44,7 @@ class TestSearchSolutions:
             pytest.fail(f'call to GetSearchSolutionsResults failed to return a valid response stream with exception {str(e)}')
 
     @staticmethod
-    def test_fully_specified_pipelines(stub: core_pb2_grpc.CoreStub, protocol_version: str, random_forest_pipeline: pipeline_module.Pipeline):
+    def test_fully_specified_pipelines(stub: core_pb2_grpc.CoreStub, protocol_version: str, random_forest_pipeline: pipeline_module.Pipeline, sick_problem: grpc_problem.ProblemDescription):
         pipeline_description: pipeline_pb2.PipelineDescription = PipelineDescription.pipeline_to_protobuf_pipeline(random_forest_pipeline)
-        response = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version, template=pipeline_description))
+        response = stub.SearchSolutions(core_pb2.SearchSolutionsRequest(version=protocol_version, template=pipeline_description, problem=sick_problem))
         assert isinstance(response, core_pb2.SearchSolutionsResponse), 'call to SearchSolutions with fully specified pipeline did not return an instance of SearchSolutionsResponse'
