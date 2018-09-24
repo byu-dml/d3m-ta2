d2e94a94b8c7cacb787b563837e836b5d3e42611
Sean George <seangeorge0@gmail.com>
2018-09-24 13:25:17 -0600

    Add extra search for queue test

--- test/test_queue.py
+++ test/test_queue.py
@@ -13,12 +13,17 @@ class TestQueue:
         low_priority_search = SearchProcess(uuid.uuid4(), search_solutions_request)
         search_solutions_request = core_pb2.SearchSolutionsRequest(priority=1)
         high_priority_search = SearchProcess(uuid.uuid4(), search_solutions_request)
+        search_solutions_request = core_pb2.SearchSolutionsRequest(priority=100)
+        highest_priority_search = SearchProcess(uuid.uuid4(), search_solutions_request)
         
         core_session.insert_into_queue(low_priority_search)
         core_session.insert_into_queue(high_priority_search)
+        core_session.insert_into_queue(highest_priority_search)
         
         work_queue = core_session.work_queue
-        
+
+        search = work_queue.get()
+        assert search.search_id == highest_priority_search.search_id, 'Search with lower priority at the top of the queue'
         search = work_queue.get()
         assert search.search_id == high_priority_search.search_id, 'Search with lower priority at the top of the queue'
         search = work_queue.get()
