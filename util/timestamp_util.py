b1c12721d7b37987e2b026161121c162904b253b
Sean George <seangeorge0@gmail.com>
2018-09-25 12:54:36 -0600

    Remove unneeded import alias

--- util/timestamp_util.py
+++ util/timestamp_util.py
@@ -1,16 +1,16 @@
 import datetime
-from google.protobuf.timestamp_pb2 import Timestamp as ProtoTimestamp
+from google.protobuf.timestamp_pb2 import Timestamp
 
 
 class TimestampUtil:
 
     @staticmethod
-    def get_proto_timestamp(created_datetime: datetime.datetime) -> ProtoTimestamp:
+    def get_proto_timestamp(created_datetime: datetime.datetime) -> Timestamp:
         created_datetime = created_datetime.replace(tzinfo=None)
-        created: ProtoTimestamp = ProtoTimestamp()
+        created: Timestamp = Timestamp()
         created.FromDatetime(created_datetime)
         return created
 
     @staticmethod
-    def get_current_proto_timestamp() -> ProtoTimestamp:
-        return ProtoTimestamp()
+    def get_current_proto_timestamp() -> Timestamp:
+        return Timestamp()
