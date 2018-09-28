7a0c4f6c183cd48facdaf5755f20c2c4842928b5
jaromrex <jaromchristensen5@gmail.com>
2018-09-28 10:49:38 -0600

    fixed error in find_search_solutions to iterate through dictionary properly

--- wrapper/pipeline_description.py
+++ wrapper/pipeline_description.py
@@ -68,13 +68,49 @@ class PipelineDescription:
                 primitive = Primitive.get_primitive_from_json(primitive_description)
                 arguments = step.arguments
                 primitive_step_arguments = PipelineDescription.get_primitive_step_arguments(arguments)
+                outputs = step.outputs
+                primitive_step_outputs = PipelineDescription.get_primitive_step_outputs(outputs)
+                hyperparams = step.hyperparams
+                primitive_step_hyperparams = PipelineDescription.get_primitive_step_hyperparams(hyperparams)
+
+
+                # TODO: hyperparams field
+                # TODO: users field (optional)
+
 
                 primitive_description_step = pipeline_pb2.PrimitivePipelineDescriptionStep(primitive=primitive,
-                                                                                           arguments=primitive_step_arguments)
+                                                                                           arguments=primitive_step_arguments,
+                                                                                           outputs=primitive_step_outputs)
 
                 pipeline_description_step = pipeline_pb2.PipelineDescriptionStep(primitive=primitive_description_step)
                 steps.append(pipeline_description_step)
         return steps
+
+    @staticmethod
+    def get_primitive_step_hyperparams(hyperparams: dict) -> typing.Dict[str, pipeline_pb2.PrimitiveStepHyperparameter]:
+        primitive_step_hyperparams = {}
+        for key, hyperparam in hyperparams.items():
+            data = hyperparam['data']
+            type = hyperparam['type']
+            primitive_step_hyperparam = None
+            if type == pipeline_module.ArgumentType.CONTAINER:
+                pass
+            if type == pipeline_module.ArgumentType.DATA:
+                pass
+            if type == pipeline_module.ArgumentType.PRIMITIVE:
+                pass
+            if type == pipeline_module.ArgumentType.VALUE:
+                pass
+
+
+    @staticmethod
+    def get_primitive_step_outputs(outputs: typing.List[str]) -> typing.List[pipeline_pb2.StepOutput]:
+        primitive_step_outputs = []
+        for output in outputs:
+            primitive_step_output = pipeline_pb2.StepOutput(id=output)
+            primitive_step_outputs.append(primitive_step_output)
+        return primitive_step_outputs
+
 
     @staticmethod
     def get_primitive_step_arguments(arguments: dict) -> typing.Dict[str, pipeline_pb2.PrimitiveStepArgument]:
@@ -82,7 +118,6 @@ class PipelineDescription:
         for key, argument in arguments.items():
             data = argument['data']
             type_ = argument['type']
-
             primitive_step_argument = None
             if type_ == pipeline_module.ArgumentType.CONTAINER:
                 container_argument = pipeline_pb2.ContainerArgument(data=data)
@@ -109,18 +144,64 @@ class PipelineDescription:
     @staticmethod
     def get_pipeline_from_protobuf_pipeline(protobuf_pipeline: pipeline_pb2.PipelineDescription) -> pipeline_module.Pipeline:
         id = protobuf_pipeline.id
-        context = PipelineDescription.proto_pipeline_context_enum_to_pipeline_context_enum(protobuf_pipeline.context)
+        # source
         # created
+        context = PipelineDescription.proto_pipeline_context_enum_to_pipeline_context_enum(protobuf_pipeline.context)
         name = protobuf_pipeline.name
         description = protobuf_pipeline.description
-
+        # TODO: users field (optional)
         pipeline = pipeline_module.Pipeline(pipeline_id=id, context=context, name=name, description=description)
 
+        # add inputs to pipeline
         for input in protobuf_pipeline.inputs:
-            pipeline.add_input(input.name)
+            pipeline.add_input(name=input.name)
 
-        # for protobuf_step in protobuf_pipeline.steps:
-        #
+        # add steps to pipeline
+        for protobuf_step in protobuf_pipeline.steps:
+            if protobuf_step.WhichOneof('step') == 'primitive':
+                protobuf_primitive_step = protobuf_step.primitive
+                protobuf_primitive_description = protobuf_primitive_step.primitive
+                primitive_description = {'id': protobuf_primitive_description.id,
+                                         'version': protobuf_primitive_description.version,
+                                         'python_path': protobuf_primitive_description.python_path,
+                                         'name': protobuf_primitive_description.name,
+                                         'digest': protobuf_primitive_description.digest
+                                        }
+                # initialize primitive step with primitive description
+                step = pipeline_module.PrimitiveStep(primitive_description=primitive_description)
+
+                # add arguments to primitive step
+                for name, protobuf_argument in protobuf_primitive_step.arguments.items():
+                    argument_type_str = protobuf_argument.WhichOneof('argument')
+                    data_reference = getattr(protobuf_argument, argument_type_str).data
+                    argument_type = pipeline_module.ArgumentType[argument_type_str.upper()]
+                    step.add_argument(name=name, argument_type=argument_type, data_reference=data_reference)
+
+                # add hyperparams to primitive step
+                # for name, protobuf_hyperparam in protobuf_primitive_step.hyperparams.items():
+                #     if protobuf_hyperparam.WhichOneof('argument') == 'container':
+                #         argument_type = pipeline_module.ArgumentType.CONTAINER
+                #         data = protobuf_hyperparam.container.data
+                #     elif protobuf_hyperparam.WhichOneof('argument') == 'data':
+                #         argument_type = pipeline_module.ArgumentType.DATA
+                #         data = protobuf_hyperparam.data.data
+                #     elif protobuf_hyperparam.WhichOneof('argument') == 'primitive':
+                #         argument_type = pipeline_module.ArgumentType.PRIMITIVE
+                #         data = protobuf_hyperparam.primitive.data
+                #     elif protobuf_hyperparam.WhichOneof('argument') == 'value':
+                #         argument_type = pipeline_module.ArgumentType.VALUE
+                #         data = protobuf_hyperparam.value.data
+                #
+                #     step.add_hyperparameter(name=name, argument_type=type, data=data)
+
+                # add outputs to primitive
+                for output in protobuf_primitive_step.outputs:
+                    step.add_output(output.id)
+
+                # TODO: add users field (options)
+
+                pipeline.add_step(step)
+
         # for output in protobuf_pipeline.outputs:
         #     pipeline.add_output(output.data, output.name)
 
